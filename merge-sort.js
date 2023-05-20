928912c66af86a4eb11ae427374ec79b4f49dad6
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-05-20 22:23:50 +1000

    refactored merge codes

--- merge-sort.js
+++ merge-sort.js
@@ -1,22 +1,22 @@
-const mergeSort = array => {
-  if (array.length <= 1) return array;
+const mergeSort = arr => {
+  if (arr.length <= 1) return arr;
 
-  const midPoint = Math.floor(array.length / 2);
-  const leftHalf = mergeSort(array.slice(0, midPoint));
-  const rightHalf = mergeSort(array.slice(midPoint));
-  return merge(leftHalf, rightHalf);
+  const midPoint = Math.floor(arr.length / 2);
+  const leftPart = mergeSort(arr.slice(0, midPoint));
+  const rightPart = mergeSort(arr.slice(midPoint));
+  return merge(leftPart, rightPart);
 };
 
-const merge = (leftArray, rightArray) => {
+const merge = (leftArr, rightArr) => {
   const mergedCollection = [];
 
-  while (leftArray.length > 0 && rightArray.length > 0) {
-    const arrayWithMin = leftArray[0] < rightArray[0] ? leftArray : rightArray;
+  while (leftArr.length > 0 && rightArr.length > 0) {
+    const arrayWithMin = leftArr[0] < rightArr[0] ? leftArr : rightArr;
     const mergeElement = arrayWithMin.shift();
     mergedCollection.push(mergeElement);
   }
 
-  return mergedCollection.concat(leftArray, rightArray);
+  return mergedCollection.concat(leftArr, rightArr);
 };
 
 console.log(mergeSort([5, 0, 10, -3, -1, 4, 12, -5]));
